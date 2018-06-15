d42954dcd78bdef6fdd863b35f9ecf69da61d00c
Takehiro Iyatomi <iyatomi@gmail.com>
2018-06-15 12:48:00 +0900

    bug fix and port setting

--- server/functions/hello/index.ts
+++ server/functions/hello/index.ts
@@ -5,5 +5,5 @@ import { sleep } from '../../common/util';
 export var hello = async (req: Request, res: Response) => {
     await sleep(req.body.wait_msec);
     res.status(200);
-    res.send("delayed hello: " + req.body.wait_msec + " ms");
+    res.send("delayed hello: " + req.body.wait_msec + " ms\n");
 };
