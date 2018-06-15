018503bfc19ebab0633297dc1f3afd95e45a4f80
Takehiro Iyatomi <iyatomi@gmail.com>
2018-06-15 13:15:36 +0900

    introduce sha3 js

--- server/functions/calc/index.ts
+++ server/functions/calc/index.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express';
 import { Config } from '../../common/config';
+const sha3_512 = require('js-sha3').sha3_512;
 
 export var calc = async (req: Request, res: Response) => {
+    res.status(200);
+    res.send(sha3_512.update(req.body.data).hex() + "\n");
 }
